01ab032164a2c3cb2747c49bdc5000bdd58981a9
Eaudran <nevosis@gmail.com>
2017-05-22 17:36:33 +0200

    add bottstrap element (navbar)

--- src/App.js
+++ src/App.js
@@ -2,45 +2,20 @@
 import React from "react";
 import "./App.css";
 import HelloWorldList from "./HelloWorldList";
+import HeaderNavBar from "./HeaderNavBar";
 
 const App = () => {
-
   return (
     <div className="App">
+      <HeaderNavBar /> 
+     
+
       <HelloWorldList />
+    
+
     </div>
   );
 };
 
-// import logo from "./logo.svg";
-//
-
-// class App extends Component {
-//   handleClick = () => {
-//     console.log("hello");
-//   };
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="col-md-6">
-//           <div className="App-header">
-//             <img src={logo} className="App-logo" alt="logo" />
-//             <h2>Welcome to React NOOOOOqqqqq</h2>
-//           </div>
-//           <p onClick={this.handleClick} className="App-intro">
-//             To get started, edit <code>src/App.js</code> and save to reload.
-//           </p>
-//         </div>
-//         <div className="col-md-2">
-//           aa
-//         </div>
-//         <div className="col-md-4">
-//           aa
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;

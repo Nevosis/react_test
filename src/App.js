3b2e424176870097ab21cd6bbb7b9974b688d872
Eaudran <nevosis@gmail.com>
2017-05-22 14:30:20 +0200

    include and test bootstrap

--- src/App.js
+++ src/App.js
@@ -11,15 +11,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React NOOOOOqqqqq</h2>
+        <div className="col-md-6">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React NOOOOOqqqqq</h2>
+          </div>
+          <p onClick={this.handleClick} className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p onClick={this.handleClick} className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="col-md-2">
+          aa
+        </div>
+        <div className="col-md-4">
+          aa
+        </div>
       </div>
-    );
+
+      );
   }
 }
 
